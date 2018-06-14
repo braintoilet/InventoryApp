2854f2ecc305fb9d33e943aff47bdbc88bb1dbfd
Jerome Grassnick <jerome.grassnick@gmail.com>
2018-06-14 13:38:07 +0200

    Changed Activity Lifecycle

---
+++ app/src/main/java/eu/grassnick/inventoryappv1/EditProductActivity.java
@@ -0,0 +1,13 @@
+package eu.grassnick.inventoryappv1;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class EditProductActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_product);
+    }
+}
