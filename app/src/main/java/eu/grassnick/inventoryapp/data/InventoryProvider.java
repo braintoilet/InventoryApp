aa85a518fc42a0094079c80afc8ad6cb6c2087c1
Jerome Grassnick <jerome.grassnick@gmail.com>
2018-06-15 05:09:54 +0200

    Changed Activity Lifecycle Created ProductProvider

---
+++ app/src/main/java/eu/grassnick/inventoryapp/data/InventoryProvider.java
@@ -0,0 +1,59 @@
+package eu.grassnick.inventoryapp.data;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+
+public class InventoryProvider extends ContentProvider {
+    private static final String TAG = "ProductProvider";
+
+    public InventoryDbHelper dbHelper;
+
+    @Override
+    public boolean onCreate() {
+        dbHelper = new InventoryDbHelper(getContext());
+        return true;
+    }
+
+    /**
+     * Perform the query for the given URI. Use the given projection, selection, selection arguments, and sort order.
+     */
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs,
+                        String sortOrder) {
+        return null;
+    }
+
+    /**
+     * Insert new data into the provider with the given ContentValues.
+     */
+    @Override
+    public Uri insert(Uri uri, ContentValues contentValues) {
+        return null;
+    }
+
+    /**
+     * Updates the data at the given selection and selection arguments, with the new ContentValues.
+     */
+    @Override
+    public int update(Uri uri, ContentValues contentValues, String selection, String[] selectionArgs) {
+        return 0;
+    }
+
+    /**
+     * Delete the data at the given selection and selection arguments.
+     */
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        return 0;
+    }
+
+    /**
+     * Returns the MIME type of data for the content URI.
+     */
+    @Override
+    public String getType(Uri uri) {
+        return null;
+    }
+}
