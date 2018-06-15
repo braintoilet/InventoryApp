db2592c85d7196ddf0c323fffdab2d424716e0e1
Jerome Grassnick <jerome.grassnick@gmail.com>
2018-06-15 05:13:18 +0200

    Changed Activity Lifecycle Created ProductProvider Reverted some changes

---
+++ app/src/main/java/eu/grassnick/inventoryapp/EditProductActivity.java
@@ -0,0 +1,13 @@
+package eu.grassnick.inventoryapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class EditProductActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_product);
+    }
+}
